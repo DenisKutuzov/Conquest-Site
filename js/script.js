62f638d785995f0a800f9f2c158119543f5527cd
Denis <108525452+DenisKutuzov@users.noreply.github.com>
2023-02-25 23:21:11 +0600

    new site

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.querySelector('.burger').addEventListener('click', function () {
+    document.querySelector('.burger').classList.toggle('active')
+  console.log(1)
+  document.querySelector('.nav-header').classList.toggle('open')
+})
